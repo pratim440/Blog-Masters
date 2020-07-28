115509a92fb46ac7a4b8a954cdd021bcbe41d0f8
pratim440 <c.pratim440@gmail.com>
2020-07-28 21:15:53 +0530

    Update regarding mobile responsiveness

--- app.js
+++ app.js
@@ -220,11 +220,11 @@ app.post("/login", function(req, res){
     });
 
 });
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 3000;
-// }
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log("Server started successfully");
 });
